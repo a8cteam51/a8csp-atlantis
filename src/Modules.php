9f5cb89e4d055dfe439922526b53f995224b5e00
Tony Hegyes <tony.hegyes@automattic.com>
2025-05-20 12:43:27 +0200

    Modules demo start

--- src/Modules.php
+++ src/Modules.php
@@ -1,1 +1,44 @@
-<?php
+<?php declare( strict_types=1 );
+
+namespace A8C\SpecialProjects\Atlantis;
+
+use A8C\SpecialProjects\Atlantis\Modules\Colophon;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Modules class.
+ *
+ * @since   1.0.0
+ * @version 1.0.0
+ */
+class Modules {
+	// region FIELDS AND CONSTANTS
+
+	/**
+	 * Colophon module.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 */
+	public Colophon $colophon;
+
+	// endregion
+
+	// region METHODS
+
+	/**
+	 * Registers the plugin settings.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @return  void
+	 */
+	public function initialize(): void {
+		$this->colophon = new Colophon();
+		$this->colophon->maybe_initialize();
+	}
+
+	// endregion
+}
