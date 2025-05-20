637cd80e9ca2b3a49150606b7aa56d8e8de66d91
Bernat Torras <bernattorras@gmail.com>
2025-05-20 13:23:43 +0200

    Added Modules settings page

--- src/Modules.php
+++ src/Modules.php
@@ -23,22 +23,76 @@ class Modules {
 	 */
 	public Colophon $colophon;
 
-	// endregion
-
-	// region METHODS
-
 	/**
-	 * Registers the plugin settings.
+	 * Initialize the Modules functionality.
 	 *
-	 * @since   1.0.0
-	 * @version 1.0.0
-	 *
-	 * @return  void
+	 * @return void
 	 */
 	public function initialize(): void {
-		$this->colophon = new Colophon();
-		$this->colophon->maybe_initialize();
+		add_action( 'admin_menu', array( $this, 'add_admin_menu' ), 11 );
 	}
 
-	// endregion
+	/**
+	 * Add the Modules page to the Atlantis menu.
+	 *
+	 * @return void
+	 */
+	public function add_admin_menu(): void {
+		if ( current_user_can( 'manage_options' ) ) {
+			add_submenu_page(
+				'a8csp-atlantis',
+				__( 'Modules', 'atlantis' ),
+				__( 'Modules', 'atlantis' ),
+				'edit_posts',
+				'atlantis-modules',
+				array( $this, 'render_page' )
+			);
+		}
+	}
+
+	/**
+	 * Render the access logs admin page.
+	 *
+	 * @return void
+	 */
+	public function render_page(): void {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'atlantis' ) );
+		}
+		?>
+		<div class="wrap">
+				<h1>Team51 Atlantis Settings</h1>
+				<form method="post" action="options.php">
+					<?php
+					settings_fields( 'atlantis_settings_group' );
+					$enabled = get_option( 'atlantis_enabled_modules', array() );
+					?>
+					<table class="form-table">
+						<tr>
+							<th scope="row">Autoupdate Filter</th>
+							<td>
+								<input type="checkbox" name="atlantis_enabled_modules[autoupdate-filter]" value="1" <?php checked( ! empty( $enabled['autoupdate-filter'] ) ); ?> />
+								Enable
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Tracking</th>
+							<td>
+								<input type="checkbox" name="atlantis_enabled_modules[tracking]" value="1" <?php checked( ! empty( $enabled['tracking'] ) ); ?> />
+								Enable
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Colophon</th>
+							<td>
+								<input type="checkbox" name="atlantis_enabled_modules[colophon]" value="1" <?php checked( ! empty( $enabled['colophon'] ) ); ?> />
+								Enable
+							</td>
+						</tr>
+					</table>
+					<?php submit_button(); ?>
+				</form>
+			</div>
+		<?php
+	}
 }
