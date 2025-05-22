87a3d18a401f07095e84265bad722f82ebd231ca
Tony Hegyes <tony.hegyes@automattic.com>
2025-05-22 16:41:35 +0200

    Clean up Colophon loading class

--- src/Modules/Colophon/Colophon.php
+++ src/Modules/Colophon/Colophon.php
@@ -14,64 +14,55 @@ defined( 'ABSPATH' ) || exit;
  * @version 1.0.0
  */
 class Colophon extends Module {
+	// region INHERITED METHODS
 
 	/**
-	 * Gets the module name.
+	 * {@inheritDoc}
 	 *
 	 * @since   1.0.0
 	 * @version 1.0.0
-	 *
-	 * @return  string  The module name.
 	 */
 	public function get_name(): string {
 		return 'Colophon';
 	}
 
 	/**
-	 * Gets the module description.
+	 * {@inheritDoc}
 	 *
 	 * @since   1.0.0
 	 * @version 1.0.0
-	 *
-	 * @return  string  The module description.
 	 */
 	public function get_description(): string {
 		return 'Having set up footer links in many partner sites, this aims to simplify the deployment of each, by using a more consistent api.';
 	}
 
 	/**
-	 * Checks if the module should be disabled.
+	 * {@inheritDoc}
 	 *
 	 * @since   1.0.0
 	 * @version 1.0.0
-	 *
-	 * @return  false|\WP_Error  False if the module should be enabled, WP_Error if it should be disabled.
 	 */
-	public function is_disabled(): false|\WP_Error {
-		return false;
+	protected function initialize(): void {
+		add_action( 'team51_credits', 'team51_credits' );
+		add_action( 'init', array( $this, 'register_shortcodes' ) );
 	}
 
+	// endregion
+
+	// region HOOKS
+
 	/**
-	 * Initializes the module components.
+	 * Registers the shortcodes.
 	 *
 	 * @since   1.0.0
 	 * @version 1.0.0
 	 *
 	 * @return  void
 	 */
-	public function initialize(): void {
-		add_action( 'team51_credits', 'team51_credits', 10, 1 );
-		add_action(
-			'init',
-			function () {
-				add_shortcode( 'team51-credits', 'team51_credits_shortcode' );
-			}
-		);
-		add_action(
-			'init',
-			function () {
-				add_shortcode( 'team51-current-year', 'team51_current_year_shortcode' );
-			}
-		);
+	public function register_shortcodes(): void {
+		add_shortcode( 'team51-credits', 'team51_credits_shortcode' );
+		add_shortcode( 'team51-current-year', 'team51_current_year_shortcode' );
 	}
+
+	// endregion
 }
--- src/Modules/Module.php
+++ src/Modules/Module.php
@@ -2,8 +2,6 @@
 
 namespace A8C\SpecialProjects\Atlantis\Modules;
 
-use WP_Error;
-
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -13,23 +11,68 @@ defined( 'ABSPATH' ) || exit;
  * @version 1.0.0
  */
 abstract class Module {
+	/**
+	 * Returns the name of the module.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @return  string
+	 */
 	abstract public function get_name(): string;
 
-	abstract public function initialize(): void;
-
+	/**
+	 * Returns the short description of the module.
+	 * Useful, e.g., for outputting on a settings page.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @return  string
+	 */
 	abstract public function get_description(): string;
 
+	/**
+	 * Returns the key used to store the module's settings in the database.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @return  string
+	 */
 	public function get_settings_key(): string {
 		return sanitize_title( $this->get_name() );
 	}
 
+	/**
+	 * Returns whether the module is disabled due to environmental constraints.
+	 * The reason for the module being disabled should be returned as a WP_Error object.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @return  false|\WP_Error
+	 */
 	public function is_disabled(): false|\WP_Error {
 		return false;
 	}
 
+	/**
+	 * Returns whether the module is active.
+	 * By default, that means that the module is enabled in the settings.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @return  bool
+	 */
 	public function is_active(): bool {
 		$settings = get_option( 'atlantis_enabled_modules', array() );
-		return isset( $settings[ $this->get_settings_key() ] ) && $settings[ $this->get_settings_key() ];
+		if ( ! isset( $settings[ $this->get_settings_key() ] ) ) {
+			return false;
+		}
+
+		return (bool) $settings[ $this->get_settings_key() ];
 	}
 
 	/**
@@ -50,10 +93,13 @@ abstract class Module {
 			add_action(
 				'admin_notices',
 				function () use ( $is_disabled ) {
-					printf(
-						'<div class="notice notice-error"><p><strong>%s</strong>: %s</p></div>',
-						esc_html( $this->get_name() ),
-						esc_html( $is_disabled->get_error_message() )
+					wp_admin_notice(
+						wp_sprintf(
+							'<strong>%s</strong>: %s',
+							esc_html( $this->get_name() ),
+							esc_html( $is_disabled->get_error_message() )
+						),
+						array( 'type' => 'error' )
 					);
 				}
 			);
@@ -62,4 +108,14 @@ abstract class Module {
 
 		$this->initialize();
 	}
+
+	/**
+	 * Initializes the module components.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @return  void
+	 */
+	abstract protected function initialize(): void;
 }
